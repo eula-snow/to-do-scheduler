a967b3884bdd3e0b4cab5c841d2f13326e73b58c
Maria Konova <arim.novak@gmail.com>
2023-02-19 19:44:18 +0000

    updates text-area color

--- js/script.js
+++ js/script.js
@@ -1,34 +1,45 @@
 $("#currentDate").text(moment().format("dddd MMM Do")); //displays current date
 let $addItem = $("#addItem");
 let $main = $(".container");
-let $textArea = $("textarea");
-
 let startBusiness = moment(09, "H");
-//function that creates a row for every hour from 9 to 5
-$(document).ready(function () {
-  while (startBusiness.hour() < 18) {
-    addRow();
-    console.log(startBusiness.hour());
-    console.log(moment().hour());
-    startBusiness.add(1, "hours");
-    if (startBusiness.hour() === moment().hour()) {
-      $textArea.css("background-color", "red");
-      console.log(55);
-    }
-  }
-});
+let index = 0;
 
-//function that creates a row with time, text area, and button
+//function that creates a row with time, text area, and save button
 function addRow() {
   $main.append(
     `<div class='row'>
       <div class='time'>${startBusiness.format("ha")}</div>
-      <textarea class="past"></textarea>
+      <textarea id="i${index}"></textarea>
       <button class='addItem'>
         <i class='fa-regular fa-floppy-disk'></i>
       </button>
     </div>`
   );
+}
+
+//function that creates a row for every hour from 9 to 5
+$(document).ready(function () {
+  while (startBusiness.hour() < 21) {
+    addRow();
+    colorUpdate();
+    index++;
+    // console.log(startBusiness.hour());
+    // console.log(moment().hour());
+    startBusiness.add(1, "hours");
+  }
+});
+
+//function that updates textarea color
+function colorUpdate() {
+  const idQuery = "#i" + index.toString();
+  // console.log(idQuery);
+  if (startBusiness.hour() === moment().hour()) {
+    $(idQuery).css("background-color", "#ff6961");
+  } else if (startBusiness.hour() < moment().hour()) {
+    $(idQuery).css("background-color", "gray");
+  } else {
+    $(idQuery).css("background-color", "#77dd77");
+  }
 }
 
 // var $tasks = $("#tasks");
