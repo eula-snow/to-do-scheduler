920308c7fa3459719531d0e409c98c517532f0c1
Maria Konova <arim.novak@gmail.com>
2023-02-19 20:42:21 +0000

    adds tasks to localStorage

--- js/script.js
+++ js/script.js
@@ -2,15 +2,15 @@ $("#currentDate").text(moment().format("dddd MMM Do")); //displays current date
 let $addItem = $("#addItem");
 let $main = $(".container");
 let startBusiness = moment(09, "H");
-let index = 0;
+//let index = 0;
 
 //function that creates a row with time, text area, and save button
-function addRow() {
+function addRow(index) {
   $main.append(
     `<div class='row'>
       <div class='time'>${startBusiness.format("ha")}</div>
       <textarea id="i${index}"></textarea>
-      <button class='addItem'>
+      <button class='addItem' id="b${index}">
         <i class='fa-regular fa-floppy-disk'></i>
       </button>
     </div>`
@@ -19,9 +19,11 @@ function addRow() {
 
 //function that creates a row for every hour from 9 to 5
 $(document).ready(function () {
-  while (startBusiness.hour() < 21) {
-    addRow();
-    colorUpdate();
+  let index = 0;
+  while (startBusiness.hour() < 18) {
+    addRow(index);
+    colorUpdate(index);
+    addTask(index);
     index++;
     // console.log(startBusiness.hour());
     // console.log(moment().hour());
@@ -30,29 +32,29 @@ $(document).ready(function () {
 });
 
 //function that updates textarea color
-function colorUpdate() {
-  const idQuery = "#i" + index.toString();
+function colorUpdate(index) {
+  const idText = "#i" + index.toString();
   // console.log(idQuery);
   if (startBusiness.hour() === moment().hour()) {
-    $(idQuery).css("background-color", "#ff6961");
+    $(idText).css("background-color", "#ff6961");
   } else if (startBusiness.hour() < moment().hour()) {
-    $(idQuery).css("background-color", "gray");
+    $(idText).css("background-color", "gray");
   } else {
-    $(idQuery).css("background-color", "#77dd77");
+    $(idText).css("background-color", "#77dd77");
   }
 }
 
-// var $tasks = $("#tasks");
+//functon to add tasks to localStorage
+function addTask(index) {
+  const idButton = "#b" + index.toString();
+  const idText = "#i" + index.toString();
+  const i = index;
+  $(idButton).click(function () {
+    var task = $(idText).val();
+    console.log(task);
+    localStorage.setItem("key" + i.toString(), task);
+  });
+}
 
-//add items to the list
-// $(document).ready(function () {
-//   $("#add").click(function () {
-//     var newItem = $("#item").val();
-//     $("ul").append(
-//       "<li>" + newItem + "<button id='delete'>complete</button> </li>"
-//     );
-//     //clear input
-//     $("#item").val("");
-//   });
-//   });
-// });
+//function to get tasks from localStorage
+function getTask() {}
